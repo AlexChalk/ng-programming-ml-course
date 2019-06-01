41f2f6a8777d567fc016fe9167500600b94f0c27
Alexander Chalk <contact@alexchalk.net>
2019-06-01 18:33:27 -0400

    Add code to calculate optimal C and sigma params

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,10 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+possible_values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+[p, q] = meshgrid(possible_values, possible_values);
+c_sigma_combinations = [p(:) q(:)];
 
+prediction_errors = [];
 
+for j = 1:length(c_sigma_combinations)
+  C_test = c_sigma_combinations(j, 1);
+  sigma_test = c_sigma_combinations(j, 2);
+  model = svmTrain(X, y, C_test, @(x1, x2) gaussianKernel(x1, x2, sigma_test));
 
+  predictions = svmPredict(model, Xval);
+  prediction_error = mean(double(predictions ~= yval));
 
+  prediction_errors(j) = prediction_error;
+end
+
+[_, number] = min(prediction_errors);
+
+C = c_sigma_combinations(number, 1);
+sigma = c_sigma_combinations(number, 2);
+
+C = 1;
+sigma = 0.1;
 
 
 % =========================================================================
