177f3f943d8721538e93c792882f51c89c065d69
Alexander Chalk <contact@alexchalk.net>
2019-06-09 13:22:38 -0400

    Add gaussian threshold calculation logic

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -24,16 +24,14 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+    tp = sum((pval < epsilon) & (yval == 1));
+    fp = sum((pval < epsilon) & (yval == 0));
+    fn = sum((pval >= epsilon) & (yval == 1));
 
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
 
-
-
-
-
-
-
-
-
+    F1 = (2 * precision * recall) / (precision + recall);
 
     % =============================================================
 
